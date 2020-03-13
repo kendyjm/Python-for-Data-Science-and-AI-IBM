81c696f382ade6471b985504a1e9b1d5dbcd794d
Kendy <kendyjm@gmail.com>
2020-03-13 17:02:01 +0100

    learned about functions, objects and classes

--- week3_Conditions_loops_functions_classes.py
+++ week3_Conditions_loops_functions_classes.py
@@ -1,6 +1,7 @@
 ######### comparison operators #########
 from operator import ne
-from symbol import for_stmt
+from statistics import multimode
+from symbol import for_stmt, while_stmt
 
 a = 6
 a == 7
@@ -45,5 +46,120 @@ i = 0
 while not (square := squares[i]).startswith("green"):
     print(f"adding {square} to newSquares")
     newSquares.append(square)
-    i = i+1
+    i = i + 1
 print(newSquares)
+
+
+######### functions #########
+def add1(a):
+    """
+    increment by 1\n
+    :param a: my comment about 'a'
+    :return: a+1
+    """
+    b = a + 1
+    return b
+
+
+c = add1(5)
+print(c)
+d = add1(8)
+print(d)
+
+help(add1)
+
+
+def mult(a, b):
+    """
+    a * b
+    :return:
+    """
+    return a * b
+
+
+print(mult(2, 5))
+print(mult(2.7, 5.9))
+print(mult(2, "TO"))
+
+
+def MJ():
+    """
+    no return
+    """
+    print("Mickael Jackson")
+
+
+MJ()
+
+
+def nowork():
+    pass
+
+
+print(nowork())
+
+
+def noworkBis():
+    return None
+
+
+print(noworkBis())
+
+
+def artistNames(*names):
+    for name in names:
+        print(name)
+
+
+artistNames("kendy", "jm")
+
+
+def pinkFloyd():
+    global claimedSales
+    claimedSales = "45m"
+    return claimedSales
+
+
+pinkFloyd()
+print(claimedSales)
+
+
+def hello():
+    print("hello")
+
+
+##### Objects and Classes #####
+# We could find out the type of an object by using the type command.
+class Circle(object  # parent object
+             ):
+    def __init__(self, radius, color = 'white'):
+        '''
+        this is a constructor
+        :param radius:
+        :param color: has a defaut value 'white'
+        '''
+        self.radius = radius
+        self.color = color
+
+
+    def toPrint(self):
+        print(self.radius, self.color)
+        None
+
+
+myCircle1 = Circle(1, 'red')
+print(myCircle1.radius)
+print(myCircle1.color)
+myCircle1.toPrint()
+myCircle1.color = "blue"
+myCircle1.toPrint()
+
+myCircle2 = Circle(2)
+myCircle2.toPrint()
+
+#  The dir function is useful for obtaining the list of data attributes and methods associated with a class.
+#  The object you're interested in is passed as an argument.
+#  The return value is a list of the objects data attributes.
+#  The attribute surrounded by underscores are for internal use, and you shouldn't have to worry about them.
+dir(myCircle2)
+print(dir(Circle))
