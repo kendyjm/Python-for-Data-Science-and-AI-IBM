3ec5d7de23e69dde2988bc186ab04d62f2981750
Kendy <kendyjm@gmail.com>
2020-03-20 18:38:53 +0100

    Loading Data with Pandas (popular library for Data analysis)

--- week4_Working_with_Data_in_Python.py
+++ week4_Working_with_Data_in_Python.py
@@ -33,3 +33,36 @@ with open('resources/test.txt', 'w') as File2:
 with open('resources/test.txt', 'r') as File2:
     print(File2.readlines())
 
+# Loading Data with Pandas (popular library for Data analysis)
+import pandas as pd
+csv_path = 'resources/test.csv'
+#dataframe = pandas.read_csv(csv_path)
+dataframe = pd.read_csv(csv_path)
+#print(dataframe.values)
+print(dataframe.head())
+
+#Using loc, iloc and ix
+#There are three ways to select data from a data frame in Pandas: loc, iloc, and ix.
+
+#loc is primarily label based;
+# when two arguments are used, you use column headers and row indexes to select the data you want. loc can also take an integer as a row or column number.
+item = dataframe.loc[0, "album"]
+print(item)
+
+#iloc
+#iloc is integer-based.
+# You use column numbers and row numbers to get rows or columns at particular positions in the data frame.
+item = dataframe.iloc[0, 1]
+print(item)
+
+#ix
+#By default, ix looks for a label. If ix doesn't find a label, it will use an integer. This means you can select data by using either column numbers and row numbers or column headers and row names using ix.
+#IMPORTANT: In Pandas version 0.20.0 and later, ix is deprecated.
+
+#Using loc and iloc for slicing
+#You can also use loc and iloc to slice data frames and assign the values to a new data frame.
+newdf = dataframe.loc[0:2, "artist":"released"]
+print(newdf.head())
+
+newdf = dataframe.iloc[0:3, 0:3]
+print(newdf.head())
